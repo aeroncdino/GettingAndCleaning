2b13e7a6fcca13117331f2db89a1a96d9a5174d9
aeroncdino <dinoaeron@gmail.com>
2022-11-05 22:34:32 +0800

    deleted some

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,5 @@
 library(dplyr) 
 
-# download zip file containing data if it hasn't already been downloaded
 zipUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 zipFile <- "UCI HAR Dataset.zip"
 
@@ -8,7 +7,6 @@ if (!file.exists(zipFile)) {
   download.file(zipUrl, zipFile, mode = "wb")
 }
 
-# unzip zip file containing data if data directory doesn't already exist
 dataPath <- "UCI HAR Dataset"
 if (!file.exists(dataPath)) {
   unzip(zipFile)
@@ -22,8 +20,7 @@ testSubjects <- read.table(file.path(dataPath, "test", "subject_test.txt"))
 testValues <- read.table(file.path(dataPath, "test", "X_test.txt"))
 testActivity <- read.table(file.path(dataPath, "test", "y_test.txt"))
 features <- read.table(file.path(dataPath, "features.txt"), as.is = TRUE)
-## note: feature names (in features[, 2]) are not unique
-##       e.g. fBodyAcc-bandsEnergy()-1,8
+
 
 
 # read activity labels
@@ -40,7 +37,7 @@ colnames(humanActivity) <- c("subject", features[, 2], "activity")
 
 columnsToKeep <- grepl("subject|activity|mean|std", colnames(humanActivity))
 
-# ... and keep data in these columns only
+
 humanActivity <- humanActivity[, columnsToKeep]
 
 humanActivity$activity <- factor(humanActivity$activity, 
@@ -51,7 +48,7 @@ humanActivityCols <- colnames(humanActivity)
 # remove special characters
 humanActivityCols <- gsub("[\\(\\)-]", "", humanActivityCols)
 
-# expand abbreviations and clean up names
+
 humanActivityCols <- gsub("^f", "frequencyDomain", humanActivityCols)
 humanActivityCols <- gsub("^t", "timeDomain", humanActivityCols)
 humanActivityCols <- gsub("Acc", "Accelerometer", humanActivityCols)
@@ -60,17 +57,15 @@ humanActivityCols <- gsub("Mag", "Magnitude", humanActivityCols)
 humanActivityCols <- gsub("Freq", "Frequency", humanActivityCols)
 humanActivityCols <- gsub("mean", "Mean", humanActivityCols)
 humanActivityCols <- gsub("std", "StandardDeviation", humanActivityCols)
-
-# correct typo
 humanActivityCols <- gsub("BodyBody", "Body", humanActivityCols)
 
-# use new labels as column names
+
 colnames(humanActivity) <- humanActivityCols
 
 humanActivityMeans <- humanActivity %>% 
   group_by(subject, activity) %>%
   summarise_each(funs(mean))
 
-# output to file "tidy_data.txt"
+
 write.table(humanActivityMeans, "tidy_data.txt", row.names = FALSE, 
             quote = FALSE)
